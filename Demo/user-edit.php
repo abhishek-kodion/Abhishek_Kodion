aed8dc5e76430ace6f67129622b34510f3e7db3e
Abhishek <abhishek.kodion@gmail.com>
2023-06-23 18:53:36 +0530

    Test task

--- Demo/user-edit.php
+++ Demo/user-edit.php
@@ -15,6 +15,96 @@ include 'includes/header.php';
 // }
 ?>
 
+<?php
+        // Define variables and set to empty values
+        $nameErr = $addressErr = $contactErr = $emailErr = $passwordErr = $confirmPasswordErr = $imageErr = "";
+        $name = $address = $contact = $email = $password = $confirmPassword = $image = "";
+
+        // Function to sanitize user input
+        function sanitizeInput($data) {
+            $data = trim($data);
+            $data = stripslashes($data);
+            $data = htmlspecialchars($data);
+            return $data;
+        }
+
+        // Handle form submission
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            // Validate name
+            if (empty($_POST["name"])) {
+                $nameErr = "Name is required";
+            } else {
+                $name = sanitizeInput($_POST["name"]);
+                // Check if name only contains letters and whitespace
+                if (!preg_match("/^[a-zA-Z ]*$/", $name)) {
+                    $nameErr = "Only letters and white space allowed";
+                }
+            }
+
+            // Validate address
+            if (empty($_POST["address"])) {
+                $addressErr = "Address is required";
+            } else {
+                $address = sanitizeInput($_POST["address"]);
+            }
+
+            // Validate contact number
+            if (empty($_POST["contact"])) {
+                $contactErr = "Contact number is required";
+            } else {
+                $contact = sanitizeInput($_POST["contact"]);
+                // Check if contact number is a valid format
+                if (!preg_match("/^[0-9]{10}$/", $contact)) {
+                    $contactErr = "Invalid contact number format";
+                }
+            }
+
+            // Validate email
+            if (empty($_POST["email"])) {
+                $emailErr = "Email is required";
+            } else {
+                $email = sanitizeInput($_POST["email"]);
+                // Check if email address is well-formed
+                if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                    $emailErr = "Invalid email format";
+                }
+            }
+
+            // Validate password
+            if (empty($_POST["password"])) {
+                $passwordErr = "Password is required";
+            } else {
+                $password = $_POST["password"];
+                // Password validation rules can be added here
+            }
+
+            // Validate confirm password
+            if (empty($_POST["confirmPassword"])) {
+                $confirmPasswordErr = "Please confirm password";
+            } else {
+                $confirmPassword = $_POST["confirmPassword"];
+                // Confirm password validation rules can be added here
+                if ($password !== $confirmPassword) {
+                    $confirmPasswordErr = "Passwords do not match";
+                }
+            }
+
+            // Validate image upload
+            if ($_FILES["image"]["error"] === 0) {
+                $allowedTypes = array("image/jpeg", "image/jpg", "image/png");
+                $imageType = $_FILES["image"]["type"];
+                if (!in_array($imageType, $allowedTypes)) {
+                    $imageErr = "Invalid image type. Only JPG, JPEG, and PNG files are allowed.";
+                } else {
+                    $image = $_FILES["image"]["name"];
+                    // Move uploaded file to desired directory
+                    move_uploaded_file($_FILES["image"]["tmp_name"], "uploads/" . $image);
+                }
+            }
+        }
+    ?>
+
+
   <body>
   <div class="container my-5 shadow bg-white rounded">
     <div class="row my-3">
